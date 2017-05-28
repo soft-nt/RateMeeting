efbb1afb274e8514054b65798d2fcb2465485992
soft-nt <n.torrent@gmail.com>
2017-05-28 12:48:18 +0200

    Creating the project structure

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+(function () {
+
+  // create
+  angular
+    .module('rate-meeting', [])
+    .controller('HomeController', [HomeController])
+    .config(['$logProvider', function ($logProvider) {
+      // set debug logging to on
+      if ($logProvider.debugEnabled) {
+        $logProvider.debugEnabled(true);
+      }
+    }]);
+
+  /**
+   * Home Controller
+   */
+  function HomeController() {
+    this.title = 'Home';
+    console.log(`${this.title} is ready!`);
+
+    this.run = async () => {
+      
+      
+      /**
+       * Insert your Outlook code here
+       */
+      
+    }
+  }
+
+  // when Office has initalized, manually bootstrap the app
+  Office.initialize = function () {
+    angular.bootstrap(document.body, ['rate-meeting']);
+  };
+
+})();
